1cdfb585940faf90d89c46620f61edf8be441d7c
Peter <petecht66@gmail.com>
2026-03-10 09:08:18 -0400

    uploading the four header files

---
+++ header-source-files/BSTNode.hpp
@@ -0,0 +1,29 @@
+
+
+#ifndef BSTNODE_HPP_
+#define BSTNODE_HPP_
+
+#include <iostream>
+#include <stdlib.h>
+#include "Species.hpp"
+using namespace std;
+
+class BSTNode{
+	friend class BST;
+	friend class Interface;
+	BSTNode *left;
+	BSTNode *right;
+	BSTNode *parent;
+	Species *animal;
+	int height;
+public:
+	BSTNode(string sp, string st, string inf);
+	BSTNode();
+	~BSTNode();
+	void printNode(bool XtraInfo);
+};
+
+
+
+
+#endif /* BSTNODE_HPP_ */
---
+++ header-source-files/Species.hpp
@@ -0,0 +1,23 @@
+#ifndef SPECIES_HPP_
+#define SPECIES_HPP_
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Species {
+	friend class BSTNode;
+	friend class BST;
+	friend class Interface;
+	string name;
+	string status;
+	string info;
+public:
+	Species(string sp, string st, string inf);
+	Species();
+};
+
+
+
+
+#endif /* SPECIES_HPP_ */
